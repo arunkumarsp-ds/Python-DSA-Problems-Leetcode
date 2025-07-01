75645332bff47e7497de48dca6a7cdcadfc5c53b
SP . Arunkumar <127293230+arunkumarsp-ds@users.noreply.github.com>
2025-07-01 23:40:32 +0530

    Add files via upload

---
+++ is subsequence.py
@@ -0,0 +1,19 @@
+
+# using two pointer:
+def isSubsequence(self, s: str, t: str) -> bool:
+    n1 = len(s)
+    n2 = len(t)
+    i = 0
+    j = 0
+
+    while i < n1 and j < n2:
+        if s[i] == t[j]:
+            i += 1
+            j += 1
+        else:
+            j += 1
+    if i == n1:
+        return True
+    else:
+        return False
+        
