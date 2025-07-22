663b6ac66d9014958c6b54ed2869dae372854c9c
SP . Arunkumar <127293230+arunkumarsp-ds@users.noreply.github.com>
2025-07-22 23:37:45 +0530

    Add files via upload

---
+++ Valid Anagram.py
@@ -0,0 +1,68 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false 
+"""
+"""
+🔹 Logic:
+A string t is an anagram of string s if all characters in s appear in t with the same frequency.
+
+1) Method 1: Sorting Approach:
+
+* To check this, we sort both strings and compare them.
+    - If both sorted strings are exactly the same, then t is an anagram of s
+    - Otherwise, it's not. """
+
+def isAnagram(s,t):
+        if sorted(s) == sorted(t): # sorted(string) will result into list
+            return True
+        else:
+            return False
+
+"""
+Time: O(n log n) → due to sorting both strings (each of length n)
+Space : O(n) as sorted() will create list """
+
+"""
+2) Method 2: Frequency Count (Optimal):
+
+1) we know if the frequency of the characters is same then it is anagram.
+
+2) so we need to store the frequency of the characters and need to compare — if both are same then it is anagram.
+
+3) to do that we can use list [0]*26 — essentially it will have 26 -0's in it as we have 26 alphabets.
+
+4) so whenever we see the character we will increment the count.
+
+5) to correctly update the count for the respective character we use ord() function. 
+
+	-  when we see char = a then our index should be 0 , so ord(char) - ord(a) will give correct index for all alphabets. """
+
+def isAnagram(s,t):
+    if len(s) != len(t):
+        return False
+
+    n = len(s)
+    count_t = [0] *26
+    count_s = [0] *26
+
+    for i in range(n):
+        count_t[ord(t[i]) -ord("a")] += 1
+        count_s[ord(s[i]) -ord("a")] += 1
+
+    if count_t == count_s:
+        return True
+    else:
+        return False
+    
+"""
+Time: O(n)
+Space: O(1) → space is constant irrespective of the string size, it is always going to be 26 """
